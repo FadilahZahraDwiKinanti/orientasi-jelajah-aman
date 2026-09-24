f11f8c12bfd7a6d9279946b6c6d58889581a0640
Fadilah Zahra Dwi Kinanti <fadilah.zahra.dwi.kinanti24084@mhs.uingusdur.ac.id>
2026-09-24 21:10:55 +0700

    feat: tambah modal tambah favorit

--- src/components/RiwayatList.tsx
+++ src/components/RiwayatList.tsx
@@ -1,15 +1,20 @@
-// components/RiwayatList.tsx 
-import { View, Text } from "react-native";   
-interface RiwayatListProps {   
-    daftarKota: string[]; 
-}   
-
-export default function RiwayatList({ daftarKota }: RiwayatListProps) {   
-    return (     
-    <View>       
-        {daftarKota.map((kota) => (         
-            <Text key={kota}>{kota}</Text>       
-        ))}     
-        </View>   
-    ); 
+// components/RiwayatList.tsx (diperbarui) 
+import { View, Text } from "react-native"; 
+import { Link } from "expo-router"; 
+  
+interface RiwayatListProps { 
+  daftarKota: string[]; 
 } 
+  
+export default function RiwayatList({ daftarKota }: RiwayatListProps) { 
+  return ( 
+    <View> 
+      {daftarKota.map((kota) => ( 
+        // Format objek pada href lolos pengecekan TypeScript Expo Router 
+        <Link key={kota} href={{ pathname: "../../detail/[kota]", params: { kota } }}> 
+          <Text>{kota}</Text> 
+        </Link> 
+      ))} 
+    </View> 
+  );
+}
